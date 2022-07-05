1cad99b6058116df75d4b6924ac5f921da45018a
= <remixos.kazakov@gmail.com>
2022-07-05 14:58:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/limetv/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.limetv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    String tutorials[]
+            = { "1",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21"
+
+    };
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        l = findViewById(R.id.channelsList);
+        adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                tutorials
+        );
+        l.setAdapter(adapter);
+
+
+    }
+}
