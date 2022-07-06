522b7eaa2dbc149a868a0f17d16bd2c9595243cf
= <remixos.kazakov@gmail.com>
2022-07-06 07:58:26 +0300

    изменил цвет navigation bar, минорные изменения

--- app/src/main/java/com/example/limetv/MainActivity.java
+++ app/src/main/java/com/example/limetv/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.limetv;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView l;
+    /*ListView l;
     String tutorials[]
             = { "1",
             "1",
@@ -34,19 +33,19 @@ public class MainActivity extends AppCompatActivity {
             "21"
 
     };
-    ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapter;*/
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        l = findViewById(R.id.channelsList);
+       /* l = findViewById(R.id.channels);
         adapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
                 tutorials
         );
-        l.setAdapter(adapter);
+        l.setAdapter(adapter);*/
 
 
     }
